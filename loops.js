dba416007e9231b21e2185b744f42dde908001ea
isaac santacruz <isaacsantacruz@mbp-de-isaac.mynetworksettings.com>
2023-04-30 12:07:09 -0400

    finished loop practice

---
+++ loops.js
@@ -0,0 +1,20 @@
+for(let i=0; i<100; i++) {
+    console.log(i);
+  }
+
+  for(let i=99; i>=0; i--) {
+    console.log(i);
+  }
+
+  for(let i=0; i<100; i+=2) {
+    console.log(i);
+  }
+
+  for(let i=98; i>=0; i-=2) {
+    console.log(i);
+  }
+
+  for(let i=1; i<100; i+=2) {
+    console.log(i);
+  }
+  
